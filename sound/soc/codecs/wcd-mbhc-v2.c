007becc507146d3bfff1f688f1db95b6e1e85d1a
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-06-15 23:35:48 -0300

    ASoC: Optimize audio codec!!
    -> Better audio quality;
    -> Louder;
    -> Clean;
    -> Safe.
    
    Modified!
    
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -2951,6 +2951,8 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 	const char *hph_switch = "qcom,msm-mbhc-hphl-swh";
 	const char *gnd_switch = "qcom,msm-mbhc-gnd-swh";
 
+	impedance_det_en = true;
+
 	pr_debug("%s: enter\n", __func__);
 
 	ret = of_property_read_u32(card->dev->of_node, hph_switch, &hph_swh);
